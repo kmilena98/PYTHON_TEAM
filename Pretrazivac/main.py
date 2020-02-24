0da88fab4cce87055e7809d9521509deaf71528d
Marko Djurisic <mdjurisic98@gmail.com>
2020-02-24 23:32:46 +0100

    #main dorada

--- Pretrazivac/main.py
+++ Pretrazivac/main.py
@@ -12,6 +12,15 @@ from paginacija import *
 #python-2.7.7-docs-html
 
 
+def menu():
+    print("*********************")
+    print("\tIzaberite opciju: ")
+    print("1. Promenite direktorijum ")
+    print("2. Unesite upit za pretragu ")
+    print("0. Izlaz")
+    print("*********************")
+    unos = input("Izaberite opciju : ")
+    return unos
 
 if __name__ == "__main__":
     print("Trenutni direktorijum je: " + os.getcwd())
@@ -19,17 +28,18 @@ if __name__ == "__main__":
     dir = input()
 
     while (not os.path.isdir(dir)):
-        print("Ne postoji uneti direktorijum")
+        print("Ne postoji uneti direktorijum,unesite novi:")
         dir = input()
 
+    if not os.path.isabs(dir):
+        dir = os.path.abspath(dir)
     parser1 = Parser()
     root = Trie()
-    #root1 = TrieNode()
-    links = []
+
     start = time.time()
     g = Graph()
     for dirpath, dirnames, files in os.walk(str(dir)):
-        print(f'Found directory: {dirpath}')
+        print(f'Pronadjen direktorijum: {dirpath}')
         for fn in files:
             if str(dirpath + '\\' + fn).endswith('.html'):
                 parsed = parser1.parse(dirpath + '\\' + fn)
@@ -43,54 +53,78 @@ if __name__ == "__main__":
                 for word in parser1.words:
                     root.insert(word,dirpath + '\\' + fn)
 
-
-
-
-
-    print(root.search("python"))
     end = time.time()
     print(end - start)
+    unos = 1
+    while unos != 0:
+        unos = menu()
+        if unos == "1":
+            print("Trenutni direktorijum je: " + os.getcwd())
+            print("Unesite direktorijum koji zelite da parsirate: ")
+            dir = input()
+
+            while (not os.path.isdir(dir)):
+                print("Ne postoji uneti direktorijum,unesite novi:")
+                dir = input()
+            if not os.path.isabs(dir):
+                dir = os.path.abspath(dir)
+            start = time.time()
+
+            for dirpath, dirnames, files in os.walk(str(dir)):
+                print(f'Pronadjen direktorijum: {dirpath}')
+                for fn in files:
+                    if str(dirpath + '\\' + fn).endswith('.html'):
+                        parsed = parser1.parse(dirpath + '\\' + fn)
+
+                        # *deo za graf
+                        p = os.path.join(dirpath, fn)
+                        p = os.path.abspath(p)
+                        g.addPage(p, parsed[0])
+
+                        print('parsiram:   ' + dirpath + '\\' + fn)
+                        for word in parser1.words:
+                            root.insert(word, dirpath + '\\' + fn)
+            end = time.time()
+            print(end - start)
+        elif unos == "2":
+            s = ParsirajU(root) # s vraca trazenu listu i niz rijeci iz upita (s,exists)
+#1
+            rjecnikZaRangiranje = rjecnikZaRang(root, s[1], s[0]) # uticaj broja reci koji se pojavljuje u datom linku
+            print("RJECNIK ZA RANGIRANJE PRE UTICAJA LINKOVA")
+            if len(rjecnikZaRangiranje)!= 0:
+                print(rjecnikZaRangiranje)
+            #2
+                uticajVrednostiLinkova(g, s[0], rjecnikZaRangiranje)
+                print("RJECNIK POSLE UTICAJA LINKOVA")
+                print(rjecnikZaRangiranje)
+            #3
+                uticajBrojaLinkova(root, g, rjecnikZaRangiranje, s[1])
+                print("RJECNIK POSLE UTICAJA LINKOVA")
+                print(rjecnikZaRangiranje)
+                # u rjecnikZaRangiranje se nalazi rjecnik, kljucevi su linkovi a vrednosti su rangovi
+
+                listaZaSortiranje = []
+                for strana in rjecnikZaRangiranje.keys():
+                    listaZaSortiranje.append(PageRang(strana,rjecnikZaRangiranje[strana]))
+
+                heap_sort(listaZaSortiranje)
+                print("Sortirani rangocvi su:")
+                for i in listaZaSortiranje:
+                    print(i.getPage(),i.getRang())
+
+                paginacija(listaZaSortiranje)
+
+            else:
+                print("Nema fajlova koji zadovoljavaju pretragu!")
+
+
+        elif unos == "0":
+            break
 
 
 
-    s = ParsirajU(root)# s vraca trazenu listu i niz rijeci iz upita (s,exists)
-    s[0].kljucevi()
-
-    if(s[0].Duzina() == 0):
-        print("Rezultat pretrage : 0")
-    else:
-        print("Rezultat pretrage : ")
-        s[0].Ispisi()
 
 
-    #1
-    rjecnikZaRangiranje = rjecnikZaRang(root, s[1], s[0]) # uticaj broja reci koji se pojavljuje u datom linku
-    print("RJECNIK ZA RANGIRANJE PRE UTICAJA LINKOVA")
-    if len(rjecnikZaRangiranje)!= 0:
-        print(rjecnikZaRangiranje)
-    #2
-        uticajVrednostiLinkova(g, s[0], rjecnikZaRangiranje)
-        print("RJECNIK POSLE UTICAJA LINKOVA")
-        print(rjecnikZaRangiranje)
-    #3
-        uticajBrojaLinkova(root, g, rjecnikZaRangiranje, s[1])
-        print("RJECNIK POSLE UTICAJA LINKOVA")
-        print(rjecnikZaRangiranje)
-        # u rjecnikZaRangiranje se nalazi rjecnik, kljucevi su linkovi a vrednosti su rangovi
-
-        listaZaSortiranje = []
-        for strana in rjecnikZaRangiranje.keys():
-            listaZaSortiranje.append(PageRang(strana,rjecnikZaRangiranje[strana]))
-
-        heap_sort(listaZaSortiranje)
-        print("Sortirani rangocvi su:")
-        for i in listaZaSortiranje:
-            print(i.getPage(),i.getRang())
-
-        paginacija(listaZaSortiranje)
-
-    else:
-        print("Nema fajlova koji zadovoljavaju pretragu!")
 
 
 
